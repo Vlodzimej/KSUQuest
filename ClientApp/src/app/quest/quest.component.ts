0f307bcd26e89b785b6b82fa4b901a78d9cd4217
Vlodzimej <amel-07@mail.ru>
2018-05-26 13:12:36 +0300

    Добавил шаблон основного ng-компонента Quest.

---
+++ ClientApp/src/app/quest/quest.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-quest',
+    templateUrl: './quest.component.html',
+    styleUrls: ['./quest.component.scss']
+})
+export class NameComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void { }
+}
